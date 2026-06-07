1bed71fb32134baaf67be58714d8883facdc36a0
Allan Varkey <amvarkey@gmail.com>
2026-06-07 16:30:31 -0600

    Upgraded to .net 10

--- F1ApiWrapper/Program.cs
+++ F1ApiWrapper/Program.cs
@@ -4,22 +4,24 @@ var openF1BaseUrl = builder.Configuration["OpenF1BaseUrl"]
 
 // Add services to the container. this is a test comment
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddHttpClient("OpenF1Api", client =>
 {
     client.BaseAddress = new Uri(openF1BaseUrl, UriKind.Absolute);
 });
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "F1 API");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
